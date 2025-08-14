67d270e26b53739ca9525c65443deb7a904eda13
Daniel Alberto Gutierrez Sandoval <daniyt2018@gmail.com>
2025-08-13 21:56:33 -0500

    nuevo archivo listo

---
+++ Estructura_datos/primera clase/codigo1.py
@@ -0,0 +1,7 @@
+#ingresar 2 valores y decir cual es el mayor
+a = int(input("ingresa el primer valor"))
+b = int(input("ingresa el segundo valor"))
+if a > b:
+  print(a, "es mayor que ", b)
+else:
+  print(b, "es msyor que ", a)
