6d9484335c56096b80f17449ee0194cc6cebeab2
Daniel Alberto Gutierrez Sandoval <daniyt2018@gmail.com>
2025-09-12 10:01:29 -0500

    array sort

---
+++ Numpy_Tutorial/NumpyArraySort/example.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+arr = np.array([3,2,0,1])
+arr1 = np.array(['banana','cereza','manzana'])
+arr2 = np.array([True, False, True])
+
+print(np.sort(arr))
+print("")
+print(np.sort(arr1))
+print("")
+print(np.sort(arr2))
